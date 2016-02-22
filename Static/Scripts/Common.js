3e26f396bcbccb037ada24380b8957e564d111b3
Chirag Lathigara <chiragl@maqsoftware.com>
2016-02-22 16:32:14 +0530

    Updated logos and navigation bar behaviour

--- Static/Scripts/Common.js
+++ Static/Scripts/Common.js
@@ -14,16 +14,26 @@ Date.prototype.format = function () {
 };
 $(document).ready(function () {
     "use strict";
+    $('html').click(function (evt) {
+        if (0 === $(evt.target).parents('#SiteNavigation').length && 0 === $(evt.target).parents('.MobileNavIcon').length && !$(evt.target).hasClass('MobileNavIcon') && $(window).width() < 599) {
+            $(".SecondlevelNav, #SiteNavigation").slideUp();
+        }
+    });
     $("#SiteNavigation > a").click(function (event) {
         event.stopPropagation();
         if ($(".MobileNavIcon").css("display") !== "none") {
             if ($(this).hasClass("HaveSubNav")) {
+                $(".SecondlevelNav").slideUp();
                 $(this).next(".SecondlevelNav").slideToggle(500);
                 return false;
             }
+            $("#SiteNavigation").slideUp();
             return true;
         }
         return true;
+    });
+    $(".SecondlevelNav > a").click(function () {
+        $(".SecondlevelNav, #SiteNavigation").slideUp();
     });
     $(".MobileNavIcon").click(function () {
         $("#SiteNavigation").slideToggle(500);
