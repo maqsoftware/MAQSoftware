f955ca72200b2f6f7f7c6f67b8d11225297919d8
Siddhesh Hindalekar <v-sihind@microsoft.com>
2019-03-01 17:31:05 +0530

    Changed 'Ami' to 'Amy'

--- js/resource.js
+++ js/resource.js
@@ -39,10 +39,10 @@ UnansweredQuestStrConst = {
 };
 
 BotPageStrConst = {
-    mobiHelpMessage: "Ask Ami in HR",
+    mobiHelpMessage: "Ask Amy in HR",
     placeHolder: "placeholder",
     askQuesMessage: "Ask a Question...",
-    openChatText: "Open to ask MOBI questions",
+    openChatText: "Open to ask Amy questions",
     closeChat: "Minimize"
 
 };
