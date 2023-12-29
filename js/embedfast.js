80184792dc3e3834ab2ddf091bcaaaea780b5fb5
Yash Shah <v-yassha@microsoft.com>
2023-12-29 19:30:27 +0530

    Fixed Partner Showcase video

--- js/embedfast.js
+++ js/embedfast.js
@@ -17,16 +17,31 @@ function resourcesClick(id) {
     var documentation_desc = document.getElementById("documentation_desc");
     var video_div = document.getElementById("demo_video");
     var img_div = document.getElementById("documentation_img");
+    var partner_video_desc = document.getElementById("partner_video_desc");
+    var partner_video_div = document.getElementById("partner_demo_video");
     if (id === "video_desc") {
         video_desc.classList.add("resources-div-click");
         documentation_desc.classList.remove("resources-div-click");
+        partner_video_desc.classList.remove("resources-div-click");
         img_div.style.display = 'none';
+        partner_video_div.style.display = 'none';
         video_div.style.display = 'block';
-    } else {
+    }
+    else if (id === "partner_video_desc") {
+        partner_video_desc.classList.add("resources-div-click");
+        documentation_desc.classList.remove("resources-div-click");
         video_desc.classList.remove("resources-div-click");
+        img_div.style.display = 'none';
+        video_div.style.display = 'none';
+        partner_video_div.style.display = 'block';
+    }
+    else {
+        video_desc.classList.remove("resources-div-click");
+        partner_video_desc.classList.remove("resources-div-click");
         documentation_desc.classList.add("resources-div-click");
         img_div.style.display = 'block';
         video_div.style.display = 'none';
+        partner_video_div.style.display = 'none';
     }
 }
 
